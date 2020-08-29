133822b1dd43d709592d3524be1de7d67177a121
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2020-08-29 20:08:56 +0100

    Update candles/candlesticks.py

--- candles/candlesticks.py
+++ candles/candlesticks.py
@@ -81,6 +81,8 @@ class CandleSticks:
         maxima = data[fields].max(axis=0).rename('max')
         instances = pd.concat([quantiles, maxima, tallies, nonzero], axis=1)
 
-        sticks = self.sticks(instances)
+        # sticks = self.sticks(instances)
 
-        return sticks
+        instances.reset_index(drop=False, inplace=True)
+
+        return instances # sticks
